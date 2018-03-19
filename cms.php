484fb7f1892914ec2c9198cdf351ec85caced88b
C0deJack <jackwoodpearce@gmail.com>
2018-03-19 16:15:58 +0000

    added PDO link and created sql statement to mysql db. Also added action links to the forms to the same page but this could be changed at a later date

--- cms.php
+++ cms.php
@@ -4,20 +4,20 @@
 
 <h3>Welcome to the Portfolio Input Page</h3>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <input type="text" name="email">email
     <input type="submit" value="update">
 </form><br>
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <input type="tel" name="tel">Telephone Number
     <input type="submit" value="update">
 </form><br>
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <input type="text" name="about">About me paragraph
     <input type="submit" value="update">
 </form><br>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <h4>Project holder 1</h4><br>
     <input type="text" name="project-img">Image file name<br>
     <input type="text" name="project-title">Project Title (max 100 characters)<br>
@@ -26,7 +26,7 @@
     <input type="submit" value="update">
 </form>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <h4>Project holder 2</h4><br>
     <input type="text" name="project-img">Image file name<br>
     <input type="text" name="project-title">Project Title (max 100 characters)<br>
@@ -35,7 +35,7 @@
     <input type="submit" value="update">
 </form>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <h4>Project holder 3</h4><br>
     <input type="text" name="project-img">Image file name<br>
     <input type="text" name="project-title">Project Title (max 100 characters)<br>
@@ -44,7 +44,7 @@
     <input type="submit" value="update">
 </form>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <h4>Project holder 4</h4><br>
     <input type="text" name="project-img">Image file name<br>
     <input type="text" name="project-title">Project Title (max 100 characters)<br>
@@ -53,7 +53,7 @@
     <input type="submit" value="update">
 </form>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <h4>Project holder 5</h4><br>
     <input type="text" name="project-img">Image file name<br>
     <input type="text" name="project-title">Project Title (max 100 characters)<br>
@@ -62,7 +62,7 @@
     <input type="submit" value="update">
 </form>
 
-<form method="post" action="#">
+<form method="post" action="cms.php">
     <h4>Project holder 6</h4><br>
     <input type="text" name="project-img">Image file name<br>
     <input type="text" name="project-title">Project Title (max 100 characters)<br>
@@ -70,3 +70,20 @@
     <input type="text" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
+
+<?php
+$db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
+    PDO::FETCH_ASSOC);
+
+$query= $db->prepare("SELECT `email`, `phone`, `about`,`deleted`
+FROM `cms` WHERE `deleted` = 0 ORDER BY `id` DESC LIMIT 1;");
+
+$name = $_POST ['name'];
+
+$query->bindParam(':name', $name);
+
+$query->execute();
+
+$result = $query->fetchAll();
