86a82fe5b72cc2441bf9266575435ac82599c17b
C0deJack <jackwoodpearce@gmail.com>
2018-03-19 16:50:52 +0000

    moved the php to the top of the page. Added variables and linked them into the from inputs. Changed the about me section to a text area.

--- cms.php
+++ cms.php
@@ -1,19 +1,34 @@
 <?php
+$db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
+    PDO::FETCH_ASSOC);
+
+$query= $db->prepare("SELECT `email`, `phone`, `about`,`deleted`
+FROM `cms` WHERE `deleted` = 0 ORDER BY `id` DESC LIMIT 1;");
+
+$query->execute();
+$result = $query->fetchAll();
+
+$email = $result [0]['email'];
+$phone = $result [0]['phone'];
+$about = $result [0]['about'];
+$del= $result [0]['deleted'];
 
 ?>
 
 <h3>Welcome to the Portfolio Input Page</h3>
 
 <form method="post" action="cms.php">
-    <input type="text" name="email">email
+    <input type="text" value="<?php echo $email; ?>" name="email">email
     <input type="submit" value="update">
 </form><br>
 <form method="post" action="cms.php">
-    <input type="tel" name="tel">Telephone Number
+    <input type="tel" value="<?php echo $phone; ?>" name="tel">Telephone Number
     <input type="submit" value="update">
 </form><br>
 <form method="post" action="cms.php">
-    <input type="text" name="about">About me paragraph
+    <textarea rows="8" cols="150"  name="about" form="about"><?php echo $about; ?></textarea>
     <input type="submit" value="update">
 </form><br>
 
@@ -71,19 +86,3 @@
     <input type="submit" value="update">
 </form>
 
-<?php
-$db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
-
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
-    PDO::FETCH_ASSOC);
-
-$query= $db->prepare("SELECT `email`, `phone`, `about`,`deleted`
-FROM `cms` WHERE `deleted` = 0 ORDER BY `id` DESC LIMIT 1;");
-
-$name = $_POST ['name'];
-
-$query->bindParam(':name', $name);
-
-$query->execute();
-
-$result = $query->fetchAll();
