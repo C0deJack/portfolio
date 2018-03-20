9a1baaa75e7c7443d9fbb482a934c2284dc4fdce
C0deJack <jackwoodpearce@gmail.com>
2018-03-20 09:57:00 +0000

    pulled data to all 6 projects but has generated a lot of extra code. This is working well now but will revisited later if there is time, to reduce code

--- cms.php
+++ cms.php
@@ -7,25 +7,86 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
 $query= $db->prepare("SELECT `email`, `phone`, `about`,`deleted`
 FROM `cms` WHERE `deleted` = 0 ORDER BY `id` DESC LIMIT 1;");
 
-$query2= $db->prepare("SELECT `project_img`, `project_title`, 
+$query1= $db->prepare("SELECT `project_img`, `project_title`,
 `project_text`, `project_link`,`project_delete` FROM `projects` WHERE 
-`project_delete` = 0 ORDER BY `id` DESC LIMIT 1;");
+`project_id` = '1' AND `project_delete` = 0;");
+
+$query2= $db->prepare("SELECT `project_img`, `project_title`,
+`project_text`, `project_link`,`project_delete` FROM `projects` WHERE 
+`project_id` = '2' AND `project_delete` = 0;");
+
+$query3= $db->prepare("SELECT `project_img`, `project_title`,
+`project_text`, `project_link`,`project_delete` FROM `projects` WHERE 
+`project_id` = '3' AND `project_delete` = 0;");
+
+$query4= $db->prepare("SELECT `project_img`, `project_title`,
+`project_text`, `project_link`,`project_delete` FROM `projects` WHERE 
+`project_id` = '4' AND `project_delete` = 0;");
+
+$query5= $db->prepare("SELECT `project_img`, `project_title`,
+`project_text`, `project_link`,`project_delete` FROM `projects` WHERE 
+`project_id` = '5' AND `project_delete` = 0;");
+
+$query6= $db->prepare("SELECT `project_img`, `project_title`,
+`project_text`, `project_link`,`project_delete` FROM `projects` WHERE 
+`project_id` = '6' AND `project_delete` = 0;");
 
 $query->execute();
+$query1->execute();
 $query2->execute();
+$query3->execute();
+$query4->execute();
+$query5->execute();
+$query6->execute();
+
 $result = $query->fetchAll();
+$result1 = $query1->fetchAll();
 $result2 = $query2->fetchAll();
+$result3 = $query3->fetchAll();
+$result4 = $query4->fetchAll();
+$result5 = $query5->fetchAll();
+$result6 = $query6->fetchAll();
 
 $email = $result [0]['email'];
 $phone = $result [0]['phone'];
 $about = $result [0]['about'];
-$del= $result [0]['deleted'];
+$del = $result [0]['deleted'];
 
-$project_img = $result2 [0]['project_img'];
-$project_title = $result2 [0]['project_title'];
-$project_text = $result2 [0]['project_text'];
-$project_link = $result2 [0]['project_link'];
-$project_delete= $result2 [0]['project_delete'];
+$project_img1 = $result1 [0]['project_img'];
+$project_title1 = $result1 [0]['project_title'];
+$project_text1 = $result1 [0]['project_text'];
+$project_link1 = $result1 [0]['project_link'];
+$project_delete1 = $result1 [0]['project_delete'];
+
+$project_img2 = $result2 [0]['project_img'];
+$project_title2 = $result2 [0]['project_title'];
+$project_text2 = $result2 [0]['project_text'];
+$project_link2 = $result2 [0]['project_link'];
+$project_delete2 = $result2 [0]['project_delete'];
+
+$project_img3 = $result3 [0]['project_img'];
+$project_title3 = $result3 [0]['project_title'];
+$project_text3 = $result3 [0]['project_text'];
+$project_link3 = $result3 [0]['project_link'];
+$project_delete3 = $result3 [0]['project_delete'];
+
+$project_img4 = $result4 [0]['project_img'];
+$project_title4 = $result4 [0]['project_title'];
+$project_text4 = $result4 [0]['project_text'];
+$project_link4 = $result4 [0]['project_link'];
+$project_delete4 = $result4 [0]['project_delete'];
+
+$project_img5 = $result5 [0]['project_img'];
+$project_title5 = $result5 [0]['project_title'];
+$project_text5 = $result5 [0]['project_text'];
+$project_link5 = $result5 [0]['project_link'];
+$project_delete5 = $result5 [0]['project_delete'];
+
+$project_img6 = $result6 [0]['project_img'];
+$project_title6 = $result6 [0]['project_title'];
+$project_text6 = $result6 [0]['project_text'];
+$project_link6 = $result6 [0]['project_link'];
+$project_delete6 = $result6 [0]['project_delete'];
 
 ?>
 
@@ -46,55 +107,55 @@ $project_delete= $result2 [0]['project_delete'];
 
 <form method="post" action="cms.php">
     <h4>Project holder 1</h4><br>
-    <input type="text" size="35" value="<?php echo $project_img; ?>" name="project-img">Image file name<br>
-    <input type="text" size="35" value="<?php echo $project_title; ?>" name="project-title">Project Title (max 100 characters)<br>
-    <input type="text" size="35" value="<?php echo $project_text; ?>" name="project-text">Project Description (max 2000 characters)<br>
-    <input type="text" size="35" value="<?php echo $project_link; ?>" name="project-link">Project link URL<br>
+    <input type="text" size="35" value="<?php echo $project_img1; ?>" name="project-img">Image file name<br>
+    <input type="text" size="35" value="<?php echo $project_title1; ?>" name="project-title">Project Title (max 100 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_text1; ?>" name="project-text">Project Description (max 2000 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_link1; ?>" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
 
 <form method="post" action="cms.php">
     <h4>Project holder 2</h4><br>
-    <input type="text" name="project-img">Image file name<br>
-    <input type="text" name="project-title">Project Title (max 100 characters)<br>
-    <input type="text" name="project-text">Project Description (max 2000 characters)<br>
-    <input type="text" name="project-link">Project link URL<br>
+    <input type="text" size="35" value="<?php echo $project_img2; ?>" name="project-img">Image file name<br>
+    <input type="text" size="35" value="<?php echo $project_title2; ?>" name="project-title">Project Title (max 100 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_text2; ?>" name="project-text">Project Description (max 2000 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_link2; ?>" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
 
 <form method="post" action="cms.php">
     <h4>Project holder 3</h4><br>
-    <input type="text" name="project-img">Image file name<br>
-    <input type="text" name="project-title">Project Title (max 100 characters)<br>
-    <input type="text" name="project-text">Project Description (max 2000 characters)<br>
-    <input type="text" name="project-link">Project link URL<br>
+    <input type="text" size="35" value="<?php echo $project_img3; ?>" name="project-img">Image file name<br>
+    <input type="text" size="35" value="<?php echo $project_title3; ?>" name="project-title">Project Title (max 100 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_text3; ?>" name="project-text">Project Description (max 2000 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_link3; ?>" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
 
 <form method="post" action="cms.php">
     <h4>Project holder 4</h4><br>
-    <input type="text" name="project-img">Image file name<br>
-    <input type="text" name="project-title">Project Title (max 100 characters)<br>
-    <input type="text" name="project-text">Project Description (max 2000 characters)<br>
-    <input type="text" name="project-link">Project link URL<br>
+    <input type="text" size="35" value="<?php echo $project_img4; ?>" name="project-img">Image file name<br>
+    <input type="text" size="35" value="<?php echo $project_title4; ?>" name="project-title">Project Title (max 100 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_text4; ?>" name="project-text">Project Description (max 2000 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_link4; ?>" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
 
 <form method="post" action="cms.php">
     <h4>Project holder 5</h4><br>
-    <input type="text" name="project-img">Image file name<br>
-    <input type="text" name="project-title">Project Title (max 100 characters)<br>
-    <input type="text" name="project-text">Project Description (max 2000 characters)<br>
-    <input type="text" name="project-link">Project link URL<br>
+    <input type="text" size="35" value="<?php echo $project_img5; ?>" name="project-img">Image file name<br>
+    <input type="text" size="35" value="<?php echo $project_title5; ?>" name="project-title">Project Title (max 100 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_text5; ?>" name="project-text">Project Description (max 2000 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_link5; ?>" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
 
 <form method="post" action="cms.php">
     <h4>Project holder 6</h4><br>
-    <input type="text" name="project-img">Image file name<br>
-    <input type="text" name="project-title">Project Title (max 100 characters)<br>
-    <input type="text" name="project-text">Project Description (max 2000 characters)<br>
-    <input type="text" name="project-link">Project link URL<br>
+    <input type="text" size="35" value="<?php echo $project_img6; ?>" name="project-img">Image file name<br>
+    <input type="text" size="35" value="<?php echo $project_title6; ?>" name="project-title">Project Title (max 100 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_text6; ?>" name="project-text">Project Description (max 2000 characters)<br>
+    <input type="text" size="35" value="<?php echo $project_link6; ?>" name="project-link">Project link URL<br>
     <input type="submit" value="update">
 </form>
 
