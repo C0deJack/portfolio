618cb9d243a9669ce7a1b71aaba6e29ebdb2e503
C0deJack <jackwoodpearce@gmail.com>
2018-03-21 10:07:27 +0000

    addition of new page for projects input only. added a link to the both pages for navigation

---
+++ projects.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="normalize.css" type="text/css" media="screen">
+    <link rel="stylesheet" type="text/css" href="cms.css">
+    <title>Portfolio Input Page</title>
+</head>
+<body>
+<h3>Welcome to the Projects Input Page</h3>
+
+<nav>
+    <a href="projects.php">Link to Projects Input Page</a>
+    <a href="cms.php">Link to About Me Input Page</a>
+</nav>
+</body>
+</html>
