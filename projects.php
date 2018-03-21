313848d812b866cdb4b7ff5588eb051c1bf0566a
C0deJack <jackwoodpearce@gmail.com>
2018-03-21 11:46:45 +0000

    added link to the db adding data in the input forms. Changed the db to not have deleted or project id colomn and insteead has 6 rows which are edited

--- projects.php
+++ projects.php
@@ -5,36 +5,54 @@ $db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
     PDO::FETCH_ASSOC);
 
-$query1= $db->prepare("SELECT `project_id`, `project_img`, 
-`project_title`,`project_text`, `project_link` FROM `projects` WHERE 
-`project_delete` = 0 ORDER BY `project_id` ASC;");
+$query1= $db->prepare("SELECT `id`, `project_img`, 
+`project_title`,`project_text`, `project_link` FROM `projects` 
+ORDER BY `id` ASC;");
 
 $query1->execute();
-
-$project_title = $_POST ['project_title'];
-$project_id= $_POST ['project_id'];
-
 $result1 = $query1->fetchAll();
+
+$query2= $db->prepare("UPDATE `projects` SET 
+`project_img`= :project_img, `project_title`= :project_title, `project_text`= :project_text, 
+`project_link`= :project_link WHERE `id` = :id;");
+
+$project_img = $_POST ['project_img'];
+$project_title = $_POST ['project_title'];
+$project_text = $_POST ['project_text'];
+$project_link = $_POST ['project_link'];
+$id = $_POST ['id'];
+
+$query2->bindParam(':project_img', $project_img);
+$query2->bindParam(':project_title', $project_title);
+$query2->bindParam(':project_text', $project_text);
+$query2->bindParam(':project_link', $project_link);
+$query2->bindParam(':id', $id);
+
+$query2->execute();
 
 function createProjectForm($result1)
 {
     $result_output = "";
     foreach ($result1 as $list) {
-        $result_output .=  "<form method='post' action='cms.php'>
-    <input type='hidden' value='" . $list['project_id'] . "'>" . $list['project_id'] . "
+        $result_output .=  "<form method='post' action='projects.php'>
+    <input type='hidden' name='id' value='" . $list['id'] . "'>" . $list['id'] . "
     <input type='text' class='project_form' size='35' value='" . $list ['project_title'] . "
-    ' name='project-title'>Project Title (max 100 characters)
-    <textarea rows='6' cols='80' class='project_form' size='35' value='" . $list ['project_text'] . "
-    ' name='project-text'></textarea>Project Description (max 2000 characters)
+    ' name='project_title'>Project Title (max 100 characters)
+    <textarea rows='6' cols='80' class='project_form' 
+    name='project_text'>" . $list ['project_text'] . "</textarea>
+    Project Description (max 2000 characters)
     <input type='text' class='project_form' size='35' value='" . $list ['project_img'] . "
-    ' name='project-img'>Image file name
+    ' name='project_img'>Image file name
     <input type='text' class='project_form' size='35' value='" . $list ['project_link'] . "
-    ' name='project-link'>Project link URL
+    ' name='project_link'>Project link URL
     <input type='submit' value='update'>
     </form>";
     }
     return $result_output;
 }
+
+$query1->execute();
+$result1 = $query1->fetchAll();
 
 ?>
 
@@ -57,6 +75,5 @@ function createProjectForm($result1)
 <?php
 
 echo createProjectForm($result1);
-var_dump($project_id, $project_title);
 
 ?>
