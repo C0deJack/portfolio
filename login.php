0acd495a701bd94f61ab3b78ecec50f472801275
C0deJack <jackwoodpearce@gmail.com>
2018-03-22 10:36:48 +0000

    adding a login page from the login project. This is a very basic page creating sessions and directing you to the cms page via user and password input

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+session_start ();
+$session = $_SESSION ['logged-in'];
+
+if ($session === 1) {
+    header("Location: cms.php");
+} elseif ( $session === 2) {
+    echo 'incorrect password or username';
+}  else {
+    echo 'Please enter your user name and password below';
+}
+?>
+
+<h3>LOGIN PAGE</h3>
+
+<form method="post" action="cms.php">
+    <input type="text" name="user-name">
+    <input type="text" name="password">
+    <input type="submit">
+</form>
