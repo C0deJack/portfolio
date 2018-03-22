df7ddf42b6361f1291b4b2254188c0757d4c90df
C0deJack <jackwoodpearce@gmail.com>
2018-03-22 10:45:32 +0000

    logout page created to destroy sessions. This also has links back to the login page and porfolio

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+$_SESSION = [];
+session_destroy();
+?>
+
+<h2>You have safely logged out</h2>
+
+<a href="login.php">Click here to log back in</a>
+
+<a href="index.php">Or click here to go back to the Portfolio</a>
