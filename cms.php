4e921522c73bc7584268b698227d1344ecee7f48
C0deJack <jackwoodpearce@gmail.com>
2018-03-23 11:33:19 +0000

    moved some code to one line following code review. Moved super globals out of functions

--- cms.php
+++ cms.php
@@ -4,8 +4,12 @@ session_start ();
 require('connect.php');
 require('checkPassword.php');
 
-$passwordIn = $_POST ['password'];
-$userIn = $_POST ['user-name'];
+$passwordIn = $_POST['password'];
+$userIn = $_POST['user-name'];
+
+$email_out = $_POST['email_out'];
+$phone_out = $_POST['phone_out'];
+$about_out = $_POST['about_out'];
 
 if (!empty($userIn && $passwordIn)) {
 
@@ -18,7 +22,7 @@ if (!empty($userIn && $passwordIn)) {
     echo verifyPassword($passwordOut, $passwordIn, $userOut);
 
 } elseif
-    ($_SESSION ['logged-in'] !== 1){
+    ($_SESSION['logged-in'] !== 1){
     header("Location: login.php");
 }
 
@@ -32,22 +36,22 @@ $query2= $db->prepare("UPDATE `cms` SET `deleted`= 1;");
 $query3= $db->prepare("INSERT INTO `cms` (`email`, `phone`, `about`)
 VALUES (:email_out, :phone_out, :about_out);");
 
-if (!empty($_POST ['email_out'] && $_POST ['phone_out'] && $_POST ['about_out'])) {
+if (!empty($email_out || $phone_out || $about_out)) {
     $query2->execute();
 }
 
-$query3->bindParam(':email_out', $_POST ['email_out']);
-$query3->bindParam(':phone_out', $_POST ['phone_out']);
-$query3->bindParam(':about_out', $_POST ['about_out']);
+$query3->bindParam(':email_out', $email_out);
+$query3->bindParam(':phone_out', $phone_out);
+$query3->bindParam(':about_out', $about_out);
 
 $query3->execute();
 $query->execute();
 
 $result = $query->fetch();
 
-$email = $result ['email'];
-$phone = $result ['phone'];
-$about = $result ['about'];
+$email = $result['email'];
+$phone = $result['phone'];
+$about = $result['about'];
 
 ?>
 <!DOCTYPE html>
@@ -65,12 +69,9 @@ $about = $result ['about'];
     </nav>
 
     <form method="post" action="cms.php">
-        <input type="text" name="email_out" size="35" value="<?php echo $email; ?>">
-        email
-        <input type="tel" name="phone_out" value="<?php echo $phone; ?>">
-        Telephone Number<br><br>
-        <textarea rows="12" cols="120"  name="about_out"><?php echo
-            $about; ?></textarea>
+        <input type="text" name="email_out" size="35" value="<?php echo $email; ?>">email
+        <input type="tel" name="phone_out" value="<?php echo $phone; ?>">Telephone Number<br><br>
+        <textarea rows="12" cols="120"  name="about_out"><?php echo $about; ?></textarea>
         <input type="submit" value="update">
     </form><br>
 
