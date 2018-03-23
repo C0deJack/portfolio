79a7e9114300fbb85135d1934e33a97a3560320f
C0deJack <jackwoodpearce@gmail.com>
2018-03-23 09:26:24 +0000

    moved connect function into its own file

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Connects to the database for all subsequent sql queries
+ *
+ * @return PDO
+ */
+function connect(){
+    $db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db;
+}
