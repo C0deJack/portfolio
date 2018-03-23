d58788e50ecd991cbfbbb108a268a11f6cdec2b4
C0deJack <jackwoodpearce@gmail.com>
2018-03-23 10:20:00 +0000

    added a space between doc block and function

--- connect.php
+++ connect.php
@@ -5,6 +5,7 @@
  *
  * @return PDO
  */
+
 function connect(){
     $db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
