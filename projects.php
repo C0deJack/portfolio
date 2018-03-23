25580d93b3ce17785df35fd7269a529b372b6aaf
C0deJack <jackwoodpearce@gmail.com>
2018-03-23 10:01:38 +0000

    removed update projects function and added a require

--- projects.php
+++ projects.php
@@ -1,6 +1,7 @@
 <?php
 
 require('connect.php');
+require('updateProjects.php');
 
 $db = connect();
 
@@ -12,29 +13,6 @@ $query1->execute();
 $result1 = $query1->fetchAll();
 
 $post = $_POST;
-
-
-/**
- * Pushes data from the projects input post up to the db
- *
- * @param $db
- *
- * @param $post
- */
-function updateProjects ($db, $post)
-{
-    $query2 = $db->prepare("UPDATE `projects` SET 
-`project_img`= :project_img, `project_title`= :project_title, `project_text`= :project_text, 
-`project_link`= :project_link WHERE `id` = :id;");
-
-    $query2->bindParam(':project_img', $post ['project_img']);
-    $query2->bindParam(':project_title', $post ['project_title']);
-    $query2->bindParam(':project_text', $post ['project_text']);
-    $query2->bindParam(':project_link', $post ['project_link']);
-    $query2->bindParam(':id', $post ['id']);
-
-    $query2->execute();
-}
 
 updateProjects($db,$post);
 
