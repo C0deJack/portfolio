a42aae9bc731d3411be84b19364bd0fe030f0f3a
C0deJack <jackwoodpearce@gmail.com>
2018-03-23 10:37:26 +0000

    added br tags for styling - i know, i know but there is no css file being created for this

--- logout.php
+++ logout.php
@@ -7,6 +7,6 @@ session_destroy();
 
 <h2>You have safely logged out</h2>
 
-<a href="login.php">Click here to log back in</a>
+<a href="login.php">Click here to log back in</a><br><br>
 
 <a href="index.php">Or click here to go back to the Portfolio</a>
