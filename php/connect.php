34169cb4ff2730849ec66a50d05ecd075821f420
C0deJack <jackwoodpearce@gmail.com>
2018-04-22 15:51:18 +0100

    changed the database name as not to match any others on the dev server

--- php/connect.php
+++ php/connect.php
@@ -7,7 +7,7 @@
  */
 
 function connect(){
-    $db = new PDO('mysql:host=127.0.0.1; dbname=jackdb', 'root');
+    $db = new PDO('mysql:host=127.0.0.1; dbname=jackWPdb', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
