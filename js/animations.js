c94f931183aef4872d783d2314a0e91085d55a2d
C0deJack <jackwoodpearce@gmail.com>
2018-05-04 16:29:11 +0100

    added smooth scroll feature and popups on the portfolio divs

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,39 @@
-var linkPopup = document.getElementById('linkPopup')
-var box = document.getElementById('temp')
+displayLinkPopup()
+smoothScroll()
+hiddenConsoleLogMessage()
 
-box.addEventListener("mouseover", function () {
-    linkPopup.classList.add('slideUp')
+function displayLinkPopup() {
+    let projectHolder = Array.from(document.getElementsByClassName('projectHolder'))
+
+    projectHolder.forEach(function (element) {
+        element.addEventListener("mouseover", function () {
+            element.children[0].children[1].classList.add('slideUp')
+        })
+        element.addEventListener("mouseout", function () {
+            element.children[0].children[1].classList.remove('slideUp')
+        })
+    })
+}
+
+function smoothScroll() {
+    let atagArray = document.querySelectorAll('a[href^="#"]')
+        atagArray.forEach(function (anchor) {
+        anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+            console.log(this.getAttribute('href'))
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
 })
+}
 
-box.addEventListener("mouseout", function () {
-    linkPopup.classList.remove('slideUp')
-})
+var module = document.getElementsByTagName('p');
+$clamp(module, {clamp: 3});
 
-
+function hiddenConsoleLogMessage() {
+    let log = Function.prototype.bind.call(console.log, console)
+    log.apply(console, ["If you like my code. Please get in contact " +
+    "to discuss your next project: jackwoodpearce@gmail.com "])
+}
